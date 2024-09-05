cf77508a967cf97fd3b11a7bdbc241ce0344e742
Catalina Arriagada <catalina.arriagada1@gmail.com>
2024-09-05 18:21:39 -0400

    env port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 //const port = 5000;
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 // Middleware
 app.use(cors({
     origin: 'http://localhost:3000', // Reemplazar con la URL del frontend
