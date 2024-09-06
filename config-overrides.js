2d2eb3f8b37df3be65dec4688a0b0a76f7a468b4
Catalina Arriagada <catalina.arriagada1@gmail.com>
2024-09-06 13:25:51 -0400

    test

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,10 @@
-module.exports = function override(config, env) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      path: require.resolve("path-browserify"),
-    };
-    return config;
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    os: require.resolve('os-browserify/browser'),
   };
-  
+
+  return config;
+};
